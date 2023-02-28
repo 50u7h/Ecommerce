af94cfc3498de9cb1e10ef5784f798a473420833
50u7h <50u7h@protonmail.com>
2023-02-28 14:03:53 +0300

    f => checkout - validation

---
+++ frontend/angular-ecommerce/src/app/validators/gcommerce-validators.spec.ts
@@ -0,0 +1,7 @@
+import { GCommerceValidators } from './gcommerce-validators';
+
+describe('GCommerceValidators', () => {
+  it('should create an instance', () => {
+    expect(new GCommerceValidators()).toBeTruthy();
+  });
+});
