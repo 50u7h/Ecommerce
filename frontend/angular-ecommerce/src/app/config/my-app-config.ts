5782480cff183db1f4f4ee2d32e30e6f61a54fce
50u7h <50u7h@protonmail.com>
2023-03-02 14:49:32 +0300

    okta login

---
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oa8jrdmxeNYjORdU5d7',
+    issuer: 'https://dev-83573795.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
