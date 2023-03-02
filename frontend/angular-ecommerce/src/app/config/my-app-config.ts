57d89c9aa7bb06634172d210c83083ec63e9ef2f
50u7h <60510752+50u7h@users.noreply.github.com>
2023-03-02 17:06:56 +0300

    Delete my-app-config.ts

--- frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  oidc: {
-    clientId: '0oa8jv8d7utktZXod5d7',
-    issuer: 'https://dev-83573795.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
-    scopes: ['openid', 'profile', 'email' ],
-  },
-};
