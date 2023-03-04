5dcedb817a6b4aae67f6f2a114198df016dcf40c
50u7h <50u7h@protonmail.com>
2023-03-04 18:47:04 +0300

    refactoring

--- backend/spring-boot-ecommerce/src/main/java/com/guney/ecommerce/controller/CheckoutController.java
+++ backend/spring-boot-ecommerce/src/main/java/com/guney/ecommerce/controller/CheckoutController.java
@@ -5,7 +5,6 @@ import com.guney.ecommerce.dto.PurchaseResponse;
 import com.guney.ecommerce.service.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
