e20b501098e2e19fcfa05c9fdc6ce5e7600b4395
50u7h <50u7h@protonmail.com>
2023-03-06 15:30:36 +0300

    angular environment configuration

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -4,14 +4,15 @@ import { Product } from '../common/product';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ProductCategory } from '../common/product-category';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  private baseUrl = 'http://localhost:8080/api/products';
+  private baseUrl = environment.gCommerceApiUrl + '/products';
 
-  private categoryUrl = 'http://localhost:8080/api/product-category';
+  private categoryUrl = environment.gCommerceApiUrl + '/product-category';
 
   constructor(private httpClient: HttpClient) {}
 
