21cd3340b537f9940156a10f71e23e160e40809b
50u7h <50u7h@protonmail.com>
2023-03-06 19:31:24 +0300

    f => payment - stripe

--- frontend/angular-ecommerce/src/environments/environment.development.ts
+++ frontend/angular-ecommerce/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   gCommerceApiUrl: 'https://localhost:8443/api',
+  stripePublishableKey:
+    'pk_test_51MieyiIZYeLuMKg8Qlv86RAle6wUGw1pTtUmLK56e815fxdA1pKIAYmKm0YrBSXPA9CHBZ0hjACS0iOBDK2zoKkO000IcqK2MU',
 };
